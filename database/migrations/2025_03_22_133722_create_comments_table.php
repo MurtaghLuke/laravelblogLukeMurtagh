2e0660aabe7de9749e2b6239b09da5d9ec275628
MurtaghLuke <l.murtagh2m@gmail.com>
2025-03-22 14:26:32 +0000

    Comment on blog posts. Created database table and model.

---
+++ database/migrations/2025_03_22_133722_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('blog_post_id')->constrained()->onDelete('cascade');  //foreign key.    /////constrainedtells Laravel that blog_post_id references the id column in the blog_posts table automatically. 
+            $table->string('name');                             ///on delete. if post is deleted, delete its comments too.
+            $table->text('comment'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
