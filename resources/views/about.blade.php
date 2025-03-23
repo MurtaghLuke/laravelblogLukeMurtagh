71f1776ba762b919f1f6be91376339ad7ec5cce8
MurtaghLuke <l.murtagh2m@gmail.com>
2025-03-23 20:36:36 +0000

    about page blade

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="w-4/5 m-auto text-center py-15">
+        <h1 class="text-6xl">
+            About Us
+        </h1>
+        <p class="text-xl py-8 text-gray-600">
+            Welcome to our website. Our mission is to provide valuable content and resources for all things Ireland.
+        </p>
+        <p class="text-lg text-gray-500">
+            Here at Discover Ireland, we aim to inspire and inform our visitors about Ireland's rich culture, history, and natural beauty.
+        </p>
+    </div>
+@endsection
