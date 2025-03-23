f29318008e787501c6caba421d68eae02cf19e38
MurtaghLuke <l.murtagh2m@gmail.com>
2025-03-23 16:44:34 +0000

    setup tailwind and mix

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Auth::routes();
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Auth::routes();
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
